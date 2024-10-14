66b71522815e0353161a54ccf69a70efefb34cb7
Bop-bili-bop <kisurin.kyrylo@lll.kpi.ua>
2024-10-14 17:20:14 +0300

    1

---
+++ calc.h
@@ -0,0 +1,34 @@
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdio.h>
+#include <math.h>
+#include "validation.h"
+#define M_PI 3.14159265358979323846
+#define to_degrees(x) (x * 180.0 / M_PI)
+double taylor_sin(double x, double epsilon)
+{
+    double sigma = x, sin = sigma;
+    unsigned n = 1;
+    do
+    {
+        sigma *= -x * x / ((n +2)* (n + 1));
+        sin += sigma;
+        n += 2;
+    }
+    while (fabs(sigma) > epsilon);
+    return sin;
+}
+double taylor_cos(double x, double epsilon)
+{
+    double sigma = 1, cos = sigma;
+    unsigned n = 1;
+    do
+    {
+        sigma *= -x * x / (2 * n * (2 * n - 1));
+        cos += sigma;
+        n++;
+    }
+    while (fabs(sigma) > epsilon);
+    return cos;
+}
+#endif
